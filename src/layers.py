d64b15957610fb42bdde35dcbceb8c19b37d9ffb
Sahil Khose <sahilkhose18@gmail.com>
2020-11-05 21:56:56 +0530

    Add Complete Stage1, Stage2

--- src/layers.py
+++ src/layers.py
@@ -96,17 +96,9 @@ class Stage1Discriminator(nn.Module):
             nn.Conv2d(3, img_dim, kernel_size=4, stride=2, padding=1, bias=True),
             nn.LeakyReLU(lr_slope, inplace=True), # TODO change slope?
 
-            nn.Conv2d(img_dim, img_dim*2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(img_dim*2),
-            nn.LeakyReLU(lr_slope, inplace=True),
-
-            nn.Conv2d(img_dim*2, img_dim*4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(img_dim*4),
-            nn.LeakyReLU(lr_slope, inplace=True),
-
-            nn.Conv2d(img_dim*4, img_dim*8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(img_dim*8),
-            nn.LeakyReLU(lr_slope, inplace=True)
+            _downsample(img_dim, img_dim*2),
+            _downsample(img_dim*2, img_dim*4),
+            _downsample(img_dim*4, img_dim*8)
         )
         self.conv1x1 = nn.Conv2d(img_dim*8+self.n_d, img_dim*8+self.n_d, kernel_size=1)
         self.final = nn.Linear(self.m_d*self.m_d*(self.n_d+(img_dim*8)),1)
@@ -120,8 +112,6 @@ class Stage1Discriminator(nn.Module):
         # text emb
         compressed = self.ff_for_text(text_emb)
         compressed = compressed[:,:,None,None].repeat(1, 1, self.m_d, self.m_d)
-
-        print(compressed.size(), enc.size())
 
         con = torch.cat((enc, compressed), dim=1)
         con = self.conv1x1(con)
@@ -142,8 +132,17 @@ class Stage2Generator(nn.Module):
         self.n_g = n_g
         self.down1 = nn.Conv2d(in_channels=3, out_channels=128, kernel_size=3, stride=1, padding=1)  # (batch, 128, 64, 64)
         self.relu1 = nn.LeakyReLU()
-        self.down2 = _downsample(128, 256, 4, 2, padding=1)  # (batch, 256, 32, 32)
-        self.down3 = _downsample(256, 512, 4, 2, padding=1)  # (batch, 512, 16, 16)
+        self.down2 = _downsample(128, 256)  # (batch, 256, 32, 32)
+        self.down3 = _downsample(256, 512)  # (batch, 512, 16, 16)
+        self.res1 = _residual(512 + self.n_g, 512 + self.n_g)  # (batch, 640, 16, 16)
+        self.res2 = _residual(512 + self.n_g, 512 + self.n_g)  # (batch, 640, 16, 16)
+        self.res3 = _residual(512 + self.n_g, 512 + self.n_g)  # (batch, 640, 16, 16)
+        self.res4 = _residual(512 + self.n_g, 512 + self.n_g)  # (batch, 640, 16, 16)
+        self.up1 = _upsample(512 + self.n_g, 512)  # (batch, 512, 32, 32)
+        self.up2 = _upsample(512, 256)  # (batch, 256, 64, 64)
+        self.up3 = _upsample(256, 128)  # (batch, 128, 128, 128)
+        self.up4 = _upsample(128, 64)   # (batch, 64, 256, 256)
+        self.conv = nn.Conv2d(64, 3, kernel_size=3, stride=1, padding=1)  # (batch, 3, 256, 256)
 
     def forward(self, c_0_hat, s1_image):
         """
@@ -155,14 +154,75 @@ class Stage2Generator(nn.Module):
 
         # downsample:
         down_out = self.down3(self.down2(self.relu1(self.down1(s1_image))))
-        c_out = c_0_hat.unsqueeze(2).unsqueeze(3).repeat(1, 1, 16, 16)
+        c_out = c_0_hat.unsqueeze(2).unsqueeze(3).repeat(1, 1, 16, 16)  # (batch, n_g, 16, 16)  # (batch, 128, 16, 16)
         # (batch, n_g) -> (batch, n_g, 1, 1) -> (batch, n_g, 16, 16) : n_g = 128
-        concat_out = torch.cat((down_out, c_out), dim=1)
+        concat_out = torch.cat((down_out, c_out), dim=1)  # (batch, n_g + 512, 16, 16) # (batch, 640, 16, 16)
+
+        res1_out = self.res1(concat_out)  # (batch, 640, 16, 16)
+
+        res2_in = res1_out + concat_out
+        res2_out = self.res2(res2_in)  # (batch, 640, 16, 16)
+
+        res3_in = res2_out + res2_in
+        res3_out = self.res3(res3_in)  # (batch, 640, 16, 16)
+
+        res4_in = res3_out + res3_in
+        res4_out = self.res4(res4_in)  # (batch, 640, 16, 16)
+
+        gen_out = self.conv(self.up4(self.up3(self.up2(self.up1(res4_out)))))  # (batch, 3, 256, 256)
+
+        return gen_out
 
 
-        return concat_out
+class Stage2Discriminator(nn.Module):
+    """
+    Stage 2 discriminator
+    """
 
-def _downsample(in_channels, out_channels, kernel_size, stride, padding):
+    def __init__(self, n_d=128, m_d=4, bert_dim=768, img_dim=256):
+        super(Stage2Discriminator, self).__init__()
+        self.n_d = n_d
+        self.m_d = m_d
+        self.bert_dim = bert_dim
+        lr_slope = 0.01
+
+        self.ff_for_text = nn.Linear(self.bert_dim, self.n_d)
+        self.down_sample = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=4, stride=2, padding=1, bias=True),  # (batch, 16, 128, 128)
+            nn.LeakyReLU(lr_slope, inplace=True),  # TODO change slope?
+            _downsample(16, 32),  # (batch, 32, 64, 64)
+            _downsample(32, 64),  # (batch, 64, 32, 32)
+            _downsample(64, 128), # (batch, 128, 16, 16)
+            _downsample(128, 256),# (batch, 256, 8, 8)
+            _downsample(256, 512),# (batch, 512, 4, 4)
+        )
+        self.conv1x1 = nn.Conv2d(img_dim*2+self.n_d, img_dim*2+self.n_d, kernel_size=1)
+        self.final = nn.Linear(self.m_d*self.m_d*(self.n_d+(img_dim*2)), 1)
+        self.sig = nn.Sigmoid()
+
+    def forward(self, text_emb, img):
+        batch_size = img.size()[0]
+
+        # image encode
+        enc = self.down_sample(img)
+        # text emb
+        compressed = self.ff_for_text(text_emb)
+        compressed = compressed[:, :, None, None].repeat(
+            1, 1, self.m_d, self.m_d)
+
+        con = torch.cat((enc, compressed), dim=1)
+        con = self.conv1x1(con)
+        return self.sig(self.final(con.flatten(start_dim=1)))
+
+
+def _residual(in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU()
+    )
+
+def _downsample(in_channels, out_channels, kernel_size=4, stride=2, padding=1):
     #TODO add layer_1 boolean argument with if else conditions
     #TODO figure out leaky relu 
     return nn.Sequential(
@@ -174,30 +234,44 @@ def _downsample(in_channels, out_channels, kernel_size, stride, padding):
 def _upsample(in_channels, out_channels, kernel_size=3, stride=1, padding=1):
     return nn.Sequential(
         nn.Upsample(scale_factor=2, mode='nearest'),
-        nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=True),
+        nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=False),
         nn.BatchNorm2d(out_channels),
         nn.ReLU()
     )
 
 if __name__ == "__main__":
-    emb = torch.randn((2, 768))
+    batch_size = 2
+    emb = torch.randn((batch_size, 768))
     ca1 = CAug(768,128,'cpu')
     generator1 = Stage1Generator()
     ca2 = CAug(768,128,'cpu')
     generator2 = Stage2Generator()
 
     discriminator1 = Stage1Discriminator()
+    discriminator2 = Stage2Discriminator()
 
 
     out_ca1 = ca1(emb)
-    print("ca1 output size: ", out_ca1.size())  # (2, 128)
+    print("ca1 output size: ", out_ca1.size())  # (batch_size, 128)
+    assert out_ca1.shape == (batch_size, 128)
     gen1 = generator1(out_ca1) 
-    print("output1 image dimensions :", gen1.size())  # (2, 3, 64, 64)
+    print("output1 image dimensions :", gen1.size())  # (batch_size, 3, 64, 64)
+    assert gen1.shape == (batch_size, 3, 64, 64)
+    print()
 
     disc1 = discriminator1(emb, gen1)
-    print("output1 discriminator", disc1.size())
+    print("output1 discriminator", disc1.size())  # (batch_size, 1)
+    assert disc1.shape == (batch_size, 1)
+    print()
 
     out_ca2 = ca2(emb)
-    print("ca2 output size: ", out_ca2.size())  # (2, 128)
+    print("ca2 output size: ", out_ca2.size())  # (batch_size, 128)
+    assert out_ca2.shape == (batch_size, 128)
     gen2 = generator2(out_ca2, gen1)
-    print("output2 image dimensions :", gen2.size())  # (2, 512, 16, 16)
+    print("output2 image dimensions :", gen2.size())  # (batch_size, 3, 256, 256)
+    assert gen2.shape == (batch_size, 3, 256, 256)
+    print()
+
+    disc2 = discriminator2(emb, gen2)
+    print("output2 discriminator", disc2.size())  # (batch_size, 1)
+    assert disc2.shape == (batch_size, 1)
