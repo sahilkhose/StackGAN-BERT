7b4d2b33d5b5a492f9d4a34ceb9c83be26db5618
Sahil Khose <sahilkhose18@gmail.com>
2020-11-18 14:40:51 +0530

    Update compare method and display specific

--- input/src/dataset_check.py
+++ input/src/dataset_check.py
@@ -16,79 +16,78 @@ print("Imports finished")
 print("__"*80)
 
 
-def annotations_image_show(bird_type, file):
-    """
-    Prints annotations, opens bird images
-    """
-    text = open(os.path.join(config.ANNOTATIONS, bird_type, file), "r").read().split('\n')[:-1]
-    [print(f"{idx}: {line}") for idx, line in enumerate(text)]
-    file = file.replace(".txt", ".jpg")
-    plt.imshow(plt.imread(os.path.join(config.IMAGE_DIR, bird_type, file)))
-    plt.show()
-
-def display():
-    """
-    Prints annotations and displays images in a for loop
-    """
-    # for bird_type in tqdm(sorted(os.listdir(config.IMAGE_DIR)), total=len(os.listdir(config.IMAGE_DIR))):
-    for bird_type in sorted(os.listdir(config.IMAGE_DIR)):
-        for file in sorted(os.listdir(os.path.join(config.ANNOTATIONS, bird_type))):
-            annotations_image_show(bird_type, file)
-            break
-        break
-
-def display_specific(bird_type_no=0, file_no=0):
+def display_specific(bird_type_no=0, file_no=0, file_idx=None):
     """
     Prints annotations and displays images of a specific bird
     """
     bird_type = sorted(os.listdir(config.IMAGE_DIR))[bird_type_no]
     file = sorted(os.listdir(os.path.join(config.ANNOTATIONS, bird_type)))[file_no]
-    annotations_image_show(bird_type, file)
 
+    if file_idx is None:
+        filename = os.path.join(bird_type, file)
+    else:
+        filenames = np.array(pickle.load(open("../data/birds/train/filenames.pickle", "rb"), encoding='latin1'))
+        filename = filenames[file_idx]
+        filename += ".txt"
 
-def compare_new_bert(file_1, file_2, emb_no=0):
-    emb_1 = torch.load(os.path.join(config.ANNOTATION_EMB,
-                            file_1, f"{emb_no}.pt"), map_location="cpu")
+    print(f"\nFile: {filename}\n")
 
-    emb_2 = torch.load(os.path.join(config.ANNOTATION_EMB,
-                                    file_2, f"{emb_no}.pt"), map_location="cpu")
+    text = open(os.path.join(config.ANNOTATIONS, filename), "r").read().split('\n')[:-1]
+    [print(f"{idx}: {line}") for idx, line in enumerate(text)]
+    filename = filename.replace(".txt", ".jpg")
+    plt.imshow(plt.imread(os.path.join(config.IMAGE_DIR, filename)))
+    plt.show()
+
+def compare_bert_emb(file_1, file_2, emb_no=0):
+    emb_1 = torch.load(os.path.join(config.ANNOTATION_EMB, file_1, f"{emb_no}.pt"), map_location="cpu")
+    emb_2 = torch.load(os.path.join(config.ANNOTATION_EMB, file_2, f"{emb_no}.pt"), map_location="cpu")
+    # print(emb_1.shape) # (1, 768)
 
     bert_sim = 1 - cosine(emb_1, emb_2)
     print(f"cosine similarity bert emb: {bert_sim:.2f}")
 
-    fig = plt.figure()
-    fig.add_subplot(1, 2, 1)
-    plt.imshow(plt.imread(os.path.join(config.IMAGE_DIR, file_1 + ".jpg")))
-    fig.add_subplot(1, 2, 2)
-    plt.imshow(plt.imread(os.path.join(config.IMAGE_DIR, file_2 + ".jpg")))
-
-    plt.show()
-
-
-def compare_embedding_quality(emb_idx_1=0, emb_idx_2=1, emb_no=0):
-    embeddings = np.array(pickle.load(open("../data/birds/train/char-CNN-RNN-embeddings.pickle", "rb"), 
-                                            encoding='latin1'))
+def compare_cnn_emb(emb_idx_1, emb_idx_2, emb_no=0):
+    embeddings = np.array(pickle.load(open("../data/birds/train/char-CNN-RNN-embeddings.pickle", "rb"), encoding='latin1'))
     # print(embeddings.shape) # (8855, 10, 1024)
     
-    filenames = np.array(pickle.load(open("../data/birds/train/filenames.pickle", "rb"), 
-                                            encoding='latin1'))
+    cnn_sim = 1 - cosine(embeddings[emb_idx_1][emb_no], embeddings[emb_idx_2][emb_no])
+    print(f"cosine similarity cnn embs: {cnn_sim:.2f}")
+
+def compare_embedding_quality(emb_idx_1=0, emb_idx_2=1, emb_no=0):
+    ###* Filenames to fetch embs:
+    filenames = np.array(pickle.load(open("../data/birds/train/filenames.pickle", "rb"), encoding='latin1'))
     # print(filenames.shape)  # (8855, )
 
+    ###* File paths:
     file_1 = filenames[emb_idx_1]
     file_2 = filenames[emb_idx_2]
 
     print(f"File 1: {file_1}")
     print(f"File 2: {file_2}\n")
 
-    cnn_sim = 1 - cosine(embeddings[emb_idx_1][emb_no], embeddings[emb_idx_2][emb_no])
+    ###* Annotations:
+    text1 = open(os.path.join(config.ANNOTATIONS, file_1+".txt"), "r").read().split('\n')[:-1]
+    text2 = open(os.path.join(config.ANNOTATIONS, file_2+".txt"), "r").read().split('\n')[:-1]
+    print("Annotation 1: ", text1[emb_no])
+    print("Annotation 2: ", text2[emb_no])
+    print()
 
-    print(f"cosine similarity cnn embs: {cnn_sim:.2f}")
-    compare_new_bert(file_1, file_2, emb_no=0)
+    ###* Cosine similarity:
+    compare_cnn_emb(emb_idx_1, emb_idx_2, emb_no=0)
+    compare_bert_emb(file_1, file_2, emb_no=0)
+
+    ###* Display images:
+    fig = plt.figure()
+    fig.add_subplot(1, 2, 1)
+    plt.imshow(plt.imread(os.path.join(config.IMAGE_DIR, file_1 + ".jpg")))
+    fig.add_subplot(1, 2, 2)
+    plt.imshow(plt.imread(os.path.join(config.IMAGE_DIR, file_2 + ".jpg")))
+    plt.show()
 
 if __name__ == "__main__":
-    # display()
-    # display_specific(bird_type_no=1, file_no=0)
+    # display_specific(bird_type_no=0, file_no=0)  # old method
+    display_specific(file_idx=0)  # new method
 
-
-    compare_embedding_quality(emb_idx_1=0, emb_idx_2=1, emb_no= 0)
-    # emb_idx < 8855, emb_no < 10
+    print("__"*80)
+    compare_embedding_quality(emb_idx_1=0, emb_idx_2=1, emb_no=0)
+    ###* emb_idx < 8855, emb_no < 10
